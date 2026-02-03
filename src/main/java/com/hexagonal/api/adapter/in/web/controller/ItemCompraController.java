ee3b6f8ef32f41523e9a3db3548a1540be6f79df
Ramon Monda <ramondriguess@gmail.com>
2026-02-02 22:22:37 -0300

    feat: adiciona request DTO e validação no endpoint de criar item de compra

--- src/main/java/com/hexagonal/api/adapter/in/web/controller/ItemCompraController.java
+++ src/main/java/com/hexagonal/api/adapter/in/web/controller/ItemCompraController.java
@@ -1,8 +1,12 @@
 package com.hexagonal.api.adapter.in.web.controller;
 
+import com.hexagonal.api.adapter.in.web.dto.CriarItemCompraRequestDTO;
 import com.hexagonal.api.adapter.in.web.dto.ItemCompraResponseDTO;
+import com.hexagonal.api.application.usecase.CriarItemCompraUseCase;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +21,11 @@ public class ItemCompraController {
     }
 
     @PostMapping(value = "/criar-item-compra")
-    public ResponseEntity<ItemCompraResponseDTO> criarItemCompra() {
+    public ResponseEntity<ItemCompraResponseDTO> criarItemCompra(
+            @RequestBody @Valid CriarItemCompraRequestDTO request) {
         try {
+
+
             // Implementar....
             return ResponseEntity.ok(null);
         } catch (Exception e) {
