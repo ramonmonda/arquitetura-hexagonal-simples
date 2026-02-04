9c87bde167d8d00ccfdd13157c9f770e2660854a
Ramon Monda <ramondriguess@gmail.com>
2026-02-04 19:28:24 -0300

    refactor: implementado criar da classe CriarItemCompraUseCase

--- src/main/java/com/hexagonal/api/application/usecase/CriarItemCompraUseCase.java
+++ src/main/java/com/hexagonal/api/application/usecase/CriarItemCompraUseCase.java
@@ -1,5 +1,9 @@
 package com.hexagonal.api.application.usecase;
 
+import com.hexagonal.api.application.usecase.input.CriarItemCompraInput;
+import com.hexagonal.api.domain.model.ItemCompra;
+import com.hexagonal.api.domain.port.out.ItemCompraRepositoryPort;
+
 public class CriarItemCompraUseCase {
 
     private final ItemCompraRepositoryPort repository;
@@ -8,7 +12,15 @@ public class CriarItemCompraUseCase {
         this.repository = repository;
     }
 
-    public ItemCompra criar(String nome, Integer quantidade) {
+    public ItemCompra criar(CriarItemCompraInput input) {
 
+        ItemCompra itemCompra = new ItemCompra(
+                null,
+                input.nome(),
+                input.quantidade(),
+                false
+        );
+
+        return repository.salvar(itemCompra);
     }
 }
