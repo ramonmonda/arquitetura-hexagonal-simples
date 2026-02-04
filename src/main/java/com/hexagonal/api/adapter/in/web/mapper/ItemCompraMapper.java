3a34b8a41f1224e7ff9e48d3dcd4950abe10864f
Ramon Monda <ramondriguess@gmail.com>
2026-02-04 19:25:43 -0300

    feat: criado metodos toCriarItemCompraInput e toCriarItemCompraResponseDTO na classe ItemCompraMapper

--- src/main/java/com/hexagonal/api/adapter/in/web/mapper/ItemCompraMapper.java
+++ src/main/java/com/hexagonal/api/adapter/in/web/mapper/ItemCompraMapper.java
@@ -1,4 +1,26 @@
 package com.hexagonal.api.adapter.in.web.mapper;
 
+import com.hexagonal.api.adapter.in.web.dto.CriarItemCompraRequestDTO;
+import com.hexagonal.api.adapter.in.web.dto.CriarItemCompraResponseDTO;
+import com.hexagonal.api.application.usecase.input.CriarItemCompraInput;
+import com.hexagonal.api.domain.model.ItemCompra;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ItemCompraMapper {
+
+    public CriarItemCompraInput toCriarItemCompraInput(CriarItemCompraRequestDTO requestDTO) {
+        return new CriarItemCompraInput(
+                requestDTO.nome(),
+                requestDTO.quantidade()
+        );
+    }
+
+    public CriarItemCompraResponseDTO toCriarItemCompraResponseDTO(ItemCompra itemCompra) {
+        return new CriarItemCompraResponseDTO(
+                itemCompra.getId(),
+                itemCompra.getNome(),
+                itemCompra.getQuantidade()
+        );
+    }
 }
