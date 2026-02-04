3994869896eedbe4c4ba4c8e0fececd40f122379
Ramon Monda <ramondriguess@gmail.com>
2026-02-04 19:27:42 -0300

    refactor: renomeia record ItemCompraResponseDTO para CriarItemCompraResponseDTO

--- src/main/java/com/hexagonal/api/adapter/in/web/dto/CriarItemCompraResponseDTO.java
+++ src/main/java/com/hexagonal/api/adapter/in/web/dto/CriarItemCompraResponseDTO.java
@@ -1,6 +1,6 @@
 package com.hexagonal.api.adapter.in.web.dto;
 
-public record ItemCompraResponseDTO(
+public record CriarItemCompraResponseDTO(
         Long id,
         String nome,
         Integer quantidade) {
