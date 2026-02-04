a47f1fb175f4165b222c52a58fb424a1544ca2c6
Ramon Monda <ramondriguess@gmail.com>
2026-02-04 18:10:34 -0300

    feat: alterado interface ItemCompraRepositoryPort

--- src/main/java/com/hexagonal/api/domain/port/out/ItemCompraRepositoryPort.java
+++ src/main/java/com/hexagonal/api/domain/port/out/ItemCompraRepositoryPort.java
@@ -1,4 +1,8 @@
 package com.hexagonal.api.domain.port.out;
 
+import com.hexagonal.api.domain.model.ItemCompra;
+
 public interface ItemCompraRepositoryPort {
+
+    ItemCompra salvar(ItemCompra itemCompra);
 }
